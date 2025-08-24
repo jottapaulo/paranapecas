132e19962b2de52a4793da66904e6b7aeb5fcf58
JOÃO PAULO FREITAS <joao270998@edu.unipar.br>
2025-08-24 20:31:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,5 @@ def remover_fundo():
 
 
 if __name__ == '__main__':
-    import os
     port = int(os.environ.get("PORT", 10000))  # Render usa a PORT, default 10000
     app.run(host="0.0.0.0", port=port)
-
